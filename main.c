287cad650a5fb5bd4170afaa0248de83a6042cec
AmizimDazora <162046917+AmizimDazora@users.noreply.github.com>
2024-09-26 20:19:41 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+// Atividade 4
+int main(){
+  int V;
+  printf("Digite um valor inteiro: ");
+  scanf("%d", &V);
+  if(V%2==0){
+    printf("O valor é par");
+  }else {
+    printf("O valor é impar");
+  }
+  return 0;
+}
